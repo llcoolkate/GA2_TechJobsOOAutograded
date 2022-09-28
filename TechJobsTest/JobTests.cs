3b76e3fba879a118a2a0da121bdf057d9782aabb
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2022-09-28 03:03:54 -0500

    latenight

--- TechJobsTest/JobTests.cs
+++ TechJobsTest/JobTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
 using TechJobsOO;
 
 namespace TechJobsTest
@@ -9,11 +10,29 @@ namespace TechJobsTest
         [TestMethod]//1
         public void TestSettingJobId()
         {
-            TechJob testJob1 = new TechJob();
-            TechJob testJob2 = new TechJob();
+            TechJob testerJob1 = new TechJob();
+            TechJob testerJob2 = new TechJob();
 
-            Assert.AreNotEqual(testJob1, testJob2, "Unique Id Test");
-            Assert.AreEqual(testJob2, testJob2, "Should have same Id");
+            Assert.AreNotEqual(testerJob1.Id, testerJob2.Id, "Unique Id Test");
+            Assert.IsTrue(testerJob2.Id - testerJob1.Id == 1, "Should be difference of 1");
+        }
+        [TestMethod] //2
+        public void TestJobConstructorSetsAllFields()
+        {
+            TechJob testJob1 = new TechJob("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Assert.AreEqual(testJob1.Name, "Product tester", "Testing Job Name");
+            Assert.AreEqual(testJob1.EmployerName.ToString(), "ACME", "Testing EmployerName Value");
+            Assert.AreEqual(testJob1.EmployerLocation.ToString(), "Desert", "Testing EmployerLocation Value");
+            Assert.AreEqual(testJob1.JobType.ToString(), "Quality control", "Testing JobType Value");
+            Assert.AreEqual(testJob1.JobCoreCompetency.ToString(), "Persistence", "Testing JobCoreCompetency Value");
+        }
+        [TestMethod]//3
+        public void TestJobsForEquality()
+        {
+            TechJob testJob1 = new TechJob("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            TechJob testJob2 = new TechJob("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            Assert.IsFalse(testJob1.Equals(testJob2), "Objects with different ID are not equal");
         }
     }
 }
