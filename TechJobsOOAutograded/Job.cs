e413cb93d06515aca44cf987f39338c2518c589b
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2022-09-28 00:49:11 -0500

    second commit

--- TechJobsOOAutograded/Job.cs
+++ TechJobsOOAutograded/Job.cs
@@ -3,10 +3,9 @@ namespace TechJobsOO
 {
     public class TechJob
     {
-        /*
+        
         public int Id { get; }
         private static int nextId = 1;
-
         public string Name { get; set; }
         public Employer EmployerName { get; set; }
         public Location EmployerLocation { get; set; }
@@ -14,9 +13,34 @@ namespace TechJobsOO
         public CoreCompetency JobCoreCompetency { get; set; }
 
         // TODO: Add the two necessary constructors.
+        public TechJob()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public TechJob(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = jobType;
+            JobCoreCompetency = jobCoreCompetency;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is TechJob job &&
+                Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
 
         // TODO: Generate Equals() and GetHashCode() methods.
 
-        */
+
     }
 }
